39dca9c7a1442e9369b949b4166cd1d23a36ec39
[Athina Kantis] <[athina.kantis@gmail.com]>
2024-11-06 17:16:24 +0200

    add logout at header

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,26 @@ import './App.css';
 import EmployeeList from './Components/EmployeeList/EmployeeList';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
+import { useState } from 'react';
 
 function App() {
-  
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  function handleClick() {
+    setIsLoggedIn(!isLoggedIn);
+  }
 
   return (
     <>
-      <Header />
+      <Header logOut={handleClick}/>
       <main>
-        <EmployeeList />
+        {isLoggedIn ? (
+          <>
+          <EmployeeList />
+          </>
+        ) : (
+          <button onClick={handleClick}>Log in</button>
+        )}
       </main>
       <Footer />
     </>
