5e8c0b898503ce364ed7b464570478dc3b1f0527
[Athina Kantis] <[athina.kantis@gmail.com]>
2024-11-12 08:47:17 +0200

    update login logic and handleClick for cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
 import './App.css';
 import EmployeeList from './Components/EmployeeList/EmployeeList';
 import Header from './Components/Header/Header';
+import Button from './Components/Button';
 import Footer from './Components/Footer/Footer';
 import { useState } from 'react';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
 
-  function handleClick() {
+  function toggleLogin() {
     setIsLoggedIn(prev => !prev);
+  }
+
+  function handleClick(id) {
+    console.log('I was clicked', id);
   }
 
   return (
     <>
-      <Header handleClick={handleClick}/>
+      <Header handleClick={toggleLogin}/>
       <main>
         {isLoggedIn ? (
           <>
-          <EmployeeList />
+          <EmployeeList handleClick={(id) => handleClick(id)}/>
           </>
         ) : (
           <div id="logInPage">
-          <button id='logInBtn' onClick={handleClick}>Log in</button>
+          <Button text={isLoggedIn ? 'Log out' : 'Log in'} handleClick={toggleLogin}/>
           </div>
         )}
       </main>
