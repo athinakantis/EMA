08b242653d6ade2d641ef09ca7ded72c0cd1a3d5
[Athina Kantis] <[athina.kantis@gmail.com]>
2024-11-13 20:38:54 +0200

    add directory for Custom Components

---
+++ src/Components/CustomComponents/Button/Button.jsx
@@ -0,0 +1,7 @@
+import './Button.css'
+
+function Button({ text, handleClick, type = 'button', role = 'primary', classes }) {
+    return <button className={classes} role={role} type={type} onClick={handleClick}>{text}</button>;
+}
+
+export default Button;
