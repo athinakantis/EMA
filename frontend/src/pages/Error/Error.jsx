5a5f3aeca4c7341fa3cc530ed1aedd8ac83f3034
Athina <athina.kantis@gmail.com>
2024-11-24 16:10:22 +0200

    update Login and Error pages and styling

--- frontend/src/pages/Error/Error.jsx
+++ frontend/src/pages/Error/Error.jsx
@@ -1,10 +1,27 @@
-function Error() {
-  return (
-    <div id='error'>
-      <h2>Oh no :( An error occurred</h2>
-      <p id='errormessage'></p>
-    </div>
-  );
+import statuscodes from '../../data/statuscodes';
+import './Error.css';
+import { Link } from 'react-router-dom';
+
+function Error({ status = 404 }) {
+    const error = statuscodes.find((i) => i.status === status);
+
+    return (
+        <section id='errorPage'>
+            <div id='errorContainer'>
+                <img
+                    src={`${
+                        import.meta.env.VITE_REACT_URL
+                    }/staffOverflow_logo.svg`}
+                    alt='staffoverflow logo'
+                    className='logo'
+                />
+                <h2>
+                    {status} {error.message}
+                </h2>
+                <Link to='/'>Back home</Link>
+            </div>
+        </section>
+    );
 }
 
 export default Error;
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,14 +1,45 @@
 import { Link } from 'react-router-dom';
 import './Login.css';
+import { useState } from 'react';
 
 function Login({}) {
-  return (
-    <div id='logInPage'>
-      <button className='logInBtn'>
-        <Link to='/employees'>Log in</Link>
-      </button>
-    </div>
-  );
+    const [user, setUser] = useState({});
+
+    function handleUser(e) {
+        const { username, password } = e.target;
+        setUser({ username: username, password: password });
+    }
+
+    return (
+        <section id='logInPage'>
+            <div id='loginContainer'>
+                <h2>Log in</h2>
+                <form>
+                    <div>
+                        <label htmlFor='username'>Username</label>
+                        <input
+                            type='text'
+                            name='username'
+                            id='username'
+                            value='admin'
+                            onChange={handleUser}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor='password'>Password</label>
+
+                        <input
+                            type='password'
+                            name='password'
+                            id='password'
+                            onChange={handleUser}
+                        />
+                    </div>
+                </form>
+                <Link to='/employees'>Log in</Link>
+            </div>
+        </section>
+    );
 }
 
 export default Login;
