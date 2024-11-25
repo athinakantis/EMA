bc28dbbabe384a09ceb78df73711db7334a10059
Athina <athina.kantis@gmail.com>
2024-11-25 20:50:24 +0200

    add initial success route

---
+++ frontend/src/pages/Success/Success.jsx
@@ -0,0 +1,12 @@
+function Success() {
+    return (
+        <section id='successPage'>
+            <div className='statusContainer success'>
+                <h2>Success!</h2>
+                <p>Message here</p>
+            </div>
+        </section>
+    );
+}
+
+export default Success;
