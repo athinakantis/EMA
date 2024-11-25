a26d4bcd451dff20f57e82ba8291bf40ecf6c1b4
Athina <athina.kantis@gmail.com>
2024-11-25 08:40:58 +0200

    format

--- frontend/src/Components/CustomComponents/Button/Button.jsx
+++ frontend/src/Components/CustomComponents/Button/Button.jsx
@@ -1,7 +1,22 @@
-import './Button.css'
+import './Button.css';
 
-function Button({ text, handleClick, type = 'button', role = 'primary', classes = 'default' }) {
-    return <button className={classes} role={role} type={type} onClick={handleClick}>{text}</button>;
+function Button({
+    text,
+    handleClick,
+    type = 'button',
+    role = 'primary',
+    classes = 'default',
+}) {
+    return (
+        <button
+            className={classes}
+            role={role}
+            type={type}
+            onClick={handleClick}
+        >
+            {text}
+        </button>
+    );
 }
 
 export default Button;
