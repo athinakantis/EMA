ffd1800e8f20fbd025076e610c851f354e7db0fa
Athina <athina.kantis@gmail.com>
2024-11-25 20:50:53 +0200

    update routes

--- frontend/src/routes/appRoutes.jsx
+++ frontend/src/routes/appRoutes.jsx
@@ -3,31 +3,41 @@ import Login from '../pages/Login/Login';
 import List from '../pages/EmployeeList/EmployeeList';
 import Root from '../pages/Root/Root';
 import Form from '../pages/Form/Form';
-import SinglePage from '../pages/SinglePage';
+import Menu from '../pages/Menu/Menu';
+import Success from '../pages/Success/Success';
+import SinglePage from '../pages/SinglePage/SinglePage';
 import { createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Root />,
+        element: <Login />,
         errorElement: <Error />,
+    },
+    {
+        path: '/home',
+        element: <Root />,
         children: [
             {
-                path: '/',
-                element: <Login />,
+                path: '/home',
+                element: <Menu />,
             },
             {
-                path: '/employees',
+                path: '/home/employees',
                 element: <List />,
             },
             {
-                path: '/add',
+                path: '/home/add',
                 element: <Form />,
             },
             {
-                path:'/employees/:id',
-                element: <SinglePage />
-            }
+                path: '/home/employees/:id',
+                element: <SinglePage />,
+            },
+            {
+                path: '/home/success',
+                element: <Success />,
+            },
         ],
     },
 ]);
