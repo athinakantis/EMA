19b6d39f0847adefd77e699ecfe9f68cbfb414cb
[Athina Kantis] <[athina.kantis@gmail.com]>
2024-11-27 17:51:33 +0200

    Refactory Employeecard and SinglePage

--- frontend/src/Components/EmployeeCard/EmployeeCard.jsx
+++ frontend/src/Components/EmployeeCard/EmployeeCard.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react';
 import './EmployeeCard.css';
 import Button from '../CustomComponents/Button/Button';
 import { calcMonthsWorked, calcYearsWorked } from '../../utils/calcTimeWorked';
-import { validateUpdateEmp } from '../../utils/validateInput';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 function EmployeeCard(props) {
@@ -25,30 +23,19 @@ function EmployeeCard(props) {
 
     const [role, setRole] = useState(initialRole);
     const [msg, setMsg] = useState('');
-    const [edit, setEdit] = useState(false);
+
     const [person, setPerson] = useState({
         department: department,
         location: location,
         salary: salary,
     });
     const deptTeamLead = teamLeads[person.department];
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     let monthsEmployed;
     let yearsEmployed = calcYearsWorked(startdate);
     if (yearsEmployed < 1) {
         monthsEmployed = calcMonthsWorked(startdate);
-    }
-
-    async function handleUpdateEmployee() {
-        try {
-            await validateUpdateEmp({ ...person });
-            const response = await axios.put(`${import.meta.env.VITE_API_URL}/employee/${id}`,{ data: {department: person.department, location: person.location, salary: person.salary, id: id}})
-            navigate(`/home/success`, { state: response.data })
-        } catch (err) {
-            console.error(err);
-            setMsg(err.message);
-        }
     }
 
     function handleRoleChange() {
@@ -68,41 +55,12 @@ function EmployeeCard(props) {
         }
     }
 
-    function handleChange(e) {
-        const { name, value } = e.target;
-        setPerson((prev) => ({ ...prev, [name]: value }));
-    }
-
-    function renderInput(field, value) {
-        return edit ? (
-            <>
-                <input
-                    type='text'
-                    name={field}
-                    value={value}
-                    onChange={handleChange}
-                />
-            </>
-        ) : (
-            <span className={field}>{value}</span>
-        );
-    }
-
     return (
         <div className={`employeeCard ${person.department}`}>
             <div className='title'>
                 <p className='emName'>
                     {firstname} {lastname}
                 </p>
-                <Button
-                    role='secondary'
-                    text={edit ? 'Save' : 'Edit'}
-                    handleClick={
-                        edit
-                            ? handleUpdateEmployee
-                            : () => setEdit((prev) => !prev)
-                    }
-                />
             </div>
             <div className='frame'>
                 <img
@@ -113,12 +71,12 @@ function EmployeeCard(props) {
                         xmlns='http://www.w3.org/2000/svg'
                         viewBox='0 0 24 24'
                         fill='#f48023'
-                        class='size-6'
+                        className='size-6'
                     >
                         <path
-                            fill-rule='evenodd'
+                            fillRule='evenodd'
                             d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z'
-                            clip-rule='evenodd'
+                            clipRule='evenodd'
                         />
                     </svg>
                 )}
@@ -127,23 +85,14 @@ function EmployeeCard(props) {
                 <p>
                     Role <span>{role}</span>
                 </p>
-
                 <p>
-                    Employment type <span>{employment_type}</span>
-                </p>
-
-                <p>Department {renderInput('department', person.department)}</p>
-                <p>Location {renderInput('location', person.location)}</p>
-                <p>Salary {renderInput('salary', person.salary)}</p>
-
-                <p>
-                    Time employed
+                    Time employed{' '}
                     <span>
                         {yearsEmployed > 1
-                            ? ` ${yearsEmployed} years`
+                            ? `${yearsEmployed} years`
                             : yearsEmployed === 1
-                            ? ` ${yearsEmployed} year`
-                            : yearsEmployed < 1 && ` ${monthsEmployed} months`}
+                            ? `${yearsEmployed} year`
+                            : `${monthsEmployed} months`}{' '}
                     </span>
                 </p>
             </div>
@@ -155,23 +104,22 @@ function EmployeeCard(props) {
                     handleClick={handleRoleChange}
                     type='button'
                     text={role === initialRole ? 'Promote' : 'Demote'}
-                    classes={role !== initialRole ? 'demote' : undefined}
                 />
                 <Button
                     text='See more'
-                    variant='secondary'
+                    role='primary'
                     handleClick={() => handleNavigate(id)}
                 />
                 {yearsEmployed % 5 === 0 && yearsEmployed > 1 && (
                     <Button
                         text='Schedule recognition meeting'
-                        className='schedule'
+                        className='schedule recognition'
                     />
                 )}
                 {monthsEmployed < 6 && (
                     <Button
                         text='Schedule probation review'
-                        className='schedule'
+                        className='schedule probation'
                     />
                 )}
             </div>
--- frontend/src/pages/SinglePage/SinglePage.jsx
+++ frontend/src/pages/SinglePage/SinglePage.jsx
@@ -1,5 +1,6 @@
 import { useParams, useNavigate, useLocation } from 'react-router-dom';
 import Button from '../../Components/CustomComponents/Button/Button';
+import { validateUpdateEmp } from '../../utils/validateInput';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './SinglePage.css';
@@ -7,14 +8,14 @@ import './SinglePage.css';
 function SinglePage() {
     const navigate = useNavigate();
     const { id } = useParams();
-    const [employee, setEmployee] = useState();
+    const [employee, setEmployee] = useState({});
     const [remove, setRemove] = useState(false);
     const [verifyRemove, setVerifyRemove] = useState(false);
+    const [edit, setEdit] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [msg, setMsg] = useState('');
 
-    function handleRemove() {
-        setVerifyRemove(true);
-    }
-
+    // Delete employee
     useEffect(() => {
         const removeEmployee = async () => {
             try {
@@ -22,7 +23,7 @@ function SinglePage() {
                     const response = await axios.delete(
                         `${import.meta.env.VITE_API_URL}/employee/${id}`
                     );
-                    navigate('/home/success', { state: response.data });
+                    navigate('/home/success', { state: response });
                 }
             } catch (err) {
                 console.error(err);
@@ -32,6 +33,7 @@ function SinglePage() {
         removeEmployee();
     }, [verifyRemove]);
 
+    // Get employee
     useEffect(() => {
         const fetchAPI = async () => {
             try {
@@ -39,30 +41,109 @@ function SinglePage() {
                     `${import.meta.env.VITE_API_URL}/employee/${id}`
                 );
                 setEmployee(response.data[0]);
+                console.log(response.data[0]);
+                setLoading(false);
             } catch (error) {
                 console.error('Error fetching data: ', error);
             }
         };
-
         fetchAPI();
     }, []);
 
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setEmployee((prev) => ({ ...prev, [name]: value }));
+    }
+
+    function renderInput(field, value) {
+        return edit ? (
+            <>
+                <input
+                    type='text'
+                    name={field}
+                    value={value}
+                    onChange={handleChange}
+                />
+            </>
+        ) : (
+            <span className={field}>{value}</span>
+        );
+    }
+
+    function handleRemove() {
+        setVerifyRemove(true);
+    }
+
+    async function handleUpdateEmployee() {
+        try {
+            await validateUpdateEmp({ ...employee });
+            const response = await axios.put(
+                `${import.meta.env.VITE_API_URL}/employee/${id}`,
+                {
+                    data: {
+                        department: employee.department,
+                        location: employee.location,
+                        salary: employee.salary,
+                        id: id,
+                    },
+                }
+            );
+            navigate(`/home/success`, { state: response.data });
+        } catch (err) {
+            console.error(err);
+            setMsg(err.message);
+        }
+    }
+
+    if (loading) {
+        return (
+            <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='1em'
+                height='1em'
+                viewBox='0 0 24 24'
+            >
+                <path
+                    fill='currentColor'
+                    d='M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z'
+                >
+                    <animateTransform
+                        attributeName='transform'
+                        dur='0.75s'
+                        repeatCount='indefinite'
+                        type='rotate'
+                        values='0 12 12;360 12 12'
+                    />
+                </path>
+            </svg>
+        );
+    }
+
     return (
         <section id='inspectEmployee'>
-            <button
-                className='btn return'
-                onClick={() => navigate(-1)}
-            >
+            <button className='btn return' onClick={() => navigate(-1)}>
                 <img
                     src={`${import.meta.env.VITE_REACT_URL}/returnIcon.svg`}
                     alt='Return icon'
                 />
-                Back to overview
+                Back to list
             </button>
             <div className='employeeCard inspect'>
-                <h2>
-                    {employee?.firstname} {employee?.lastname}
-                </h2>
+                <div className='title'>
+                    <h2>
+                        {employee?.firstname} {employee?.lastname}
+                    </h2>
+                    <Button
+                        role='secondary edit'
+                        handleClick={
+                            edit
+                                ? handleUpdateEmployee
+                                : () => setEdit((prev) => !prev)
+                        }
+                        img={`${import.meta.env.VITE_REACT_URL}/edit.svg`}
+                        imgAlt='Edit'
+                    />
+                </div>
                 <div className='frame'>
                     <img
                         src={`https://robohash.org/${employee?.firstname}${employee?.lastname}.png?set=set5&size=175x175`}
@@ -74,26 +155,19 @@ function SinglePage() {
                     </p>
 
                     <p>
-                        Employment type <span>{employee?.employment_type}</span>
-                    </p>
-
-                    <p>
-                        Department <span>{employee?.department}</span>
+                        Department
+                        {renderInput('department', employee?.department)}
                     </p>
                     <p>
-                        Location <span>{employee?.location}</span>
+                        Location {renderInput('location', employee?.location)}
                     </p>
-                    <p>
-                        Salary{' '}
-                        <span className='salary'>{employee?.salary}</span>
-                    </p>
+                    <p>Salary {renderInput('salary', employee?.salary)}</p>
                 </div>
 
+                {msg && <p className='error'>{msg}</p>}
+
                 {!remove ? (
-                    <Button
-                        text='Remove'
-                        handleClick={() => setRemove(true)}
-                    />
+                    <Button text='Remove' handleClick={() => setRemove(true)} />
                 ) : (
                     <div className='removeContainer'>
                         <p className='removeWarning'>
